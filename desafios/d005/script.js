121af3abf073d0c1f4b4a4056090103e0cc86450
Romário Gomes <95441690+rmrgomes@users.noreply.github.com>
2021-12-15 14:08:04 -0300

    desafio005

---
+++ desafios/d005/script.js
@@ -0,0 +1,21 @@
+function converter() {
+    var medida = document.getElementById('medida')
+    var metros = prompt('Digite uma distância em metros (m)')
+
+    var km = metros / 1000
+    var hm = metros / 100
+    var dam = metros / 10
+    var dm = metros / 0.1
+    var cm =  metros / 0.01
+    var mm =  metros / 0.001
+
+    medida.innerHTML = `A distância de ${metros}, corresponde a... <br>`
+
+    medida.innerHTML += `${km.toFixed(3)} quilômetros (Km) <br>`
+    medida.innerHTML += `${hm.toFixed(3)} hectômetros (Hm) <br>`
+    medida.innerHTML += `${dam.toFixed(3)} hectômetros (Dam) <br>`
+    medida.innerHTML += `${dm} hectômetros (dm) <br>`
+    medida.innerHTML += `${cm} hectômetros (cm) <br>`
+    medida.innerHTML += `${mm} hectômetros (mm) <br>`
+
+}
