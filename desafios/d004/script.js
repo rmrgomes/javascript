11a0236865da0a36325bbb9de8eb0740a90721d9
Romário Gomes <95441690+rmrgomes@users.noreply.github.com>
2021-12-15 13:16:02 -0300

    correção d004

--- desafios/d004/script.js
+++ desafios/d004/script.js
@@ -3,6 +3,6 @@ function calcula() {
     var preço = Number(prompt(`Quanto custa ${produto}, que você está comprando?`))
     var valor = Number(prompt('Qual foi o valor que você deu para pagar o mouse gamer?'))
    
-    var troco = preço - valor
+    var troco = valor - preço
     alert(` Você comprou ${produto} que custou R$ ${preço}. \n Deu R$ ${valor} em dinheiro e vai receber R$ ${troco} de troco. \n Volte Sempre!`)
 }
