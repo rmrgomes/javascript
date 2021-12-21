2f3e21417739361bcfe7959606ec1ff05475376d
Romário Gomes <95441690+rmrgomes@users.noreply.github.com>
2021-12-21 14:41:31 -0300

    ex018

--- exercicios/aula16ex/ex018/script.js
+++ exercicios/aula16ex/ex018/script.js
@@ -1,4 +1,4 @@
-let num = Number(document.querySelector('#num').value)
+let num = document.querySelector('#num')
 let ana = document.querySelector('#ana')
 let res= document.querySelector('#res')
 let valores = []
@@ -21,11 +21,37 @@ function inLista(n, l) {
 
 
 function adicionar() {
-    if(isNumero(num.value) && !inLista(num.value, valores)) {
-        
+    if(isNumero(num.value) && !inLista(num.value, valores)) { 
+        // ! significa que não está na lista
+        valores.push(Number(num.value))
+        let item = document.createElement('option')
+        item.text = `Valor ${num.value} adicionado.`
+        ana.appendChild(item)
     } else {
         alert('Valor inválido ou já encontrado na lista.')
     }
     num.value = ''
     num.focus()
+}
+
+function finalizar() {
+    if(valores.length == 0) {
+        alert('Adicione valores antes de finalizar!')
+    } else {
+        let total = valores.length
+        let maiorNumero = Math.max.apply(null, valores)
+        let menorNumero = Math.min.apply(null, valores)
+        let soma = 0
+        let media = 0
+        for(let pos in valores){
+            soma += valores[pos]
+        }
+        media = soma/total
+        res.innerHTML = ''
+        res.innerHTML += `<p>Ao todo, temos ${total} números cadastrados.</p>`
+        res.innerHTML += `<p>O maior número encontrado na lista é ${maiorNumero}.`
+        res.innerHTML += `<p>O menor número encontrado na lista é ${menorNumero}.`
+        res.innerHTML += `<p>Somando todos os valores, temos ${soma}.`
+        res.innerHTML += `<p>A média dos valores digitados é ${media}.</p>`
+    }
 }
